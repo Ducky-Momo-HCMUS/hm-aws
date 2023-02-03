00723030eec948dc242f9eb796716716bac2f07f
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-04 00:26:21 +0700

    Switch to sh (2)

--- certbot/scripts/setup.sh
+++ certbot/scripts/setup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -z "$DOMAIN" ]]; then
+if [ -n "${DOMAIN+1}" ]; then
     echo "Missing DOMAIN environment variable" 1>&2
     exit 1
 fi
 
-certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains $DOMAIN
+certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains ${DOMAIN}
