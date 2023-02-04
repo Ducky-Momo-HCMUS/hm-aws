b8821a27ab0b942794f90c90037ea922c348b366
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-04 21:33:27 +0700

    Update certbot

--- certbot/scripts/setup.sh
+++ certbot/scripts/setup.sh
@@ -9,4 +9,11 @@ if [[ -z "$DOMAIN" ]]; then
   exit 1;
 fi
 
-certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+fullchain_path=/etc/letsencrypt/live/"$DOMAIN"/fullchain.pem;
+privkey_path=/etc/letsencrypt/live/"$DOMAIN"/privkey.pem;
+
+if [[ -f "$fullchain_path" && -f "$privkey_path" ]]; then
+  certbot certonly --non-interactive --standalone --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+else
+  certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+fi  
