0061a9605ccb64c8a039ba4500ad18c0fbfa91bd
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-04 23:12:09 +0700

    Test nginx (2)

--- nginx/scripts/setup.sh
+++ nginx/scripts/setup.sh
@@ -9,18 +9,23 @@ if [[ -z "$DOMAIN" ]]; then
   exit 1
 fi
 
-fullchain_path=/etc/letsencrypt/live/"$DOMAIN"/fullchain.pem
-privkey_path=/etc/letsencrypt/live/"$DOMAIN"/privkey.pem
+echo "Setting up certbot"
 
-start_nginx() {
-  nginx -g "daemon off;"
-}
+fullchain_path=/etc/letsencrypt/live/$DOMAIN/fullchain.pem
+privkey_path=/etc/letsencrypt/live/$DOMAIN/privkey.pem
 
-if [[ -f "$fullchain_path" && -f "$privkey_path" ]]; then
-  echo "Certificates not found. Executing certbot --nginx"
-  certbot --nginx --non-interactive --standalone --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
-  start_nginx
-else
-  start_nginx
-  certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
-fi
+echo "$fullchain_path"
+echo "$privkey_path"
+
+certbot --nginx --non-interactive --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+nginx -g "daemon off;"
+
+# if [[ -f "$fullchain_path" && -f "$privkey_path" ]]; then
+#   echo "Certificates not found. Executing certbot --nginx"
+#   certbot --nginx --non-interactive --standalone --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+#   nginx -g "daemon off;"
+# else
+#   echo "File found"
+#   nginx -g "daemon off;"
+#   certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot/ --agree-tos --register-unsafely-without-email --domains "${DOMAIN}"
+# fi
