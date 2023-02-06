c4640558fedd0d3ae29d2e170b50a5c6ffc4ca5c
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 20:06:20 +0700

    Testing 05_sync_with_efs.sh

--- .platform/hooks/prebuild/05_sync_with_efs.sh
+++ .platform/hooks/prebuild/05_sync_with_efs.sh
@@ -2,16 +2,28 @@
 
 DOMAIN=$(/opt/elasticbeanstalk/bin/get-config environment -k DOMAIN)
 EFS_SSL_DIR=$(/opt/elasticbeanstalk/bin/get-config environment -k EFS_SSL_DIR)
+EC2_SSL_BACKUP_DIR=$(/opt/elasticbeanstalk/bin/get-config environment -k EC2_SSL_BACKUP_DIR)
 
-cp_dir() {
-  echo '$(ls -A "$1")'
-  if [ "$(ls -A "$1")" ]; then
-    echo "$1 is not empty"
+# Copy directory from $1 (source) to $2 (target) if target is empty
+cp_dir_if_target_is_empty() {
+  source=$1
+  target=$2
+
+  if [ -f "$source" ] || [ -f "$target" ]; then
+    >&2 echo "ERROR: $source and $target must be a directory"
+    exit 1
+  fi
+  # Create directory if not exist
+  mkdir -p "$source"
+
+  if [ -z "$(ls -A "$target")" ]; then
+    echo "$target is empty, copying from $source"
+    cp -R "$source/." "$target/"
   else
-    echo "$1 is empty empty"
+    echo "$target is not empty, do nothing"
   fi
 }
 
-cp_dir "${EFS_SSL_DIR}"
-
 # Sync certbot certificates
+cp_dir_if_target_is_empty "${EFS_SSL_DIR}" "${EC2_SSL_DIR}"
+cp_dir_if_target_is_empty "${EC2_SSL_DIR}" "${EC2_SSL_BACKUP_DIR}"
