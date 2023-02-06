4ee40f2d122ac9b71afd4c6c7d9de0322a1cc612
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 17:24:02 +0700

    Update 05_sync_with_efs.sh

--- .platform/hooks/prebuild/05_sync_with_efs.sh
+++ .platform/hooks/prebuild/05_sync_with_efs.sh
@@ -5,10 +5,10 @@ EFS_SSL_DIR=$(/opt/elasticbeanstalk/bin/get-config environment -k EFS_SSL_DIR)
 
 cp_dir() {
   echo '$(ls -A "$1")'
-  if [ -z '$(ls -A "$1")' ]; then
-    echo "$1 is empty"
-  else
+  if [ "$(ls -A "$1")" ]; then
     echo "$1 is not empty"
+  else
+    echo "$1 is empty empty"
   fi
 }
 
