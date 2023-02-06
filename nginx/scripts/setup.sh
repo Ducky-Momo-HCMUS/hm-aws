7186759b0dfaa369d676354bec35cfb9be99efef
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 16:47:08 +0700

    Update setup.sh

--- nginx/scripts/setup.sh
+++ nginx/scripts/setup.sh
@@ -4,15 +4,10 @@ set -e
 
 trap exit INT TERM
 
-check_env() {
-  if [[ -z "$1" ]]; then
-    echo "$1 is not set"
-    exit 1
-  fi
-}
-
-check_env DOMAIN
-check_env SOME_ENV_VAR
+if [[ -z "$DOMAIN" ]]; then
+  echo "DOMAIN is not set"
+  exit 1
+fi
 
 fullchain_path="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
 privkey_path="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
