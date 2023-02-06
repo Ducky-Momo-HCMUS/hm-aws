00b62cdcb913d10673172a2793f0d991a039f140
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 14:28:03 +0700

    Update 02_set_constant.sh (1)

--- .platform/hooks/prebuild/02_set_constant.sh
+++ .platform/hooks/prebuild/02_set_constant.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-export EFS_ID="fs-0fd3fd850a9d21c1a"
-export EFS_MOUNT_DIR="/efs"
-export EFS_DOMAIN_SSL_DIR="${EFS_MOUNT_DIR}/ssl/${DOMAIN}"
+export_env() {
+    echo 'export $1' >> ~/.bashrc
+}
 
-export EC2_SSL_DIR="/etc/ssl/letsencrypt"
+export_env 'EFS_ID="fs-0fd3fd850a9d21c1a"'
+export_env 'EFS_MOUNT_DIR="/efs"'
+export_env 'EFS_DOMAIN_SSL_DIR="${EFS_MOUNT_DIR}/ssl/${DOMAIN}"'
+
+export_env 'EC2_SSL_DIR="/etc/ssl/letsencrypt"'
+
+cat ~/.bashrc
