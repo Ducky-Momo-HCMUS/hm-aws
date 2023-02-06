03a567237331ea23a0e9f4152ec3fee3ea54fbd9
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 17:09:44 +0700

    Create 05_sync_with_efs.sh

---
+++ .platform/hooks/prebuild/05_sync_with_efs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOMAIN=$(/opt/elasticbeanstalk/bin/get-config environment -k DOMAIN)
+EFS_SSL_DIR=$(/opt/elasticbeanstalk/bin/get-config environment -k EFS_SSL_DIR)
+
+cp_dir() {
+  if [ -z '$(ls -A "$1")' ]; then
+    echo "$1 is empty"
+  else
+    echo "$1 is not empty"
+  fi
+}
+
+cp_dir EFS_SSL_DIR
+
+# Sync certbot certificates
