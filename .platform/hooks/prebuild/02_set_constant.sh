b6d13d3ec8df93236ffff8621c5dd4349f71a590
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 14:58:16 +0700

    Replace 02_set_constant.sh with .ebextensions/options.config

--- .platform/hooks/prebuild/02_set_constant.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'export EFS_ID="fs-0fd3fd850a9d21c1a"' >> ~/.bashrc
-echo 'export EFS_MOUNT_DIR="/efs"' >> ~/.bashrc
-echo 'export EFS_DOMAIN_SSL_DIR="${EFS_MOUNT_DIR}/ssl/${DOMAIN}"' >> ~/.bashrc
-
-echo 'export EC2_SSL_DIR="/etc/ssl/letsencrypt"' >> ~/.bashrc
-
-cat ~/.bashrc
