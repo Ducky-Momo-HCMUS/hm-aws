c5e8c36c53d9bbdfe2a2b7f047f2b00d3a4b97bb
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-06 16:51:21 +0700

    Add DOMAIN to nginx docker-compose

--- nginx/scripts/setup.sh
+++ nginx/scripts/setup.sh
@@ -9,8 +9,8 @@ if [[ -z "$DOMAIN" ]]; then
   exit 1
 fi
 
-fullchain_path='/etc/letsencrypt/live/"$DOMAIN"/fullchain.pem'
-privkey_path='/etc/letsencrypt/live/"$DOMAIN"/privkey.pem'
+fullchain_path="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
+privkey_path="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
 
 echo "$fullchain_path"
 echo "$privkey_path"
