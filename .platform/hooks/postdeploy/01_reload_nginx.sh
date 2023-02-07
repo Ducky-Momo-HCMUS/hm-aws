27a55ccfb85872d2e3f91b23ad73c8de443d11bd
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-08 01:46:55 +0700

    Create 01_reload_nginx.sh

---
+++ .platform/hooks/postdeploy/01_reload_nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+nginx_name="current_nginx_1"
+certbot_name="current_certbot_1"
+
+# Reload nginx to apply certbot certificates
+echo "Waiting for certbot to finish"
+until [ "$( docker inspect -f {{.State.Status}} $certbot_name )"=="exited" ]; do
+    sleep 5;
+done;
+
+echo "Certbot has exited. Reloading nginx"
+
+docker exec -it $nginx_name nginx -s reload
