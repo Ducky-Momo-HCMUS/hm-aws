f9b481442481d90b9d8bc9ebc17b3675c7ef7b10
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-07 22:55:15 +0700

    Revamp certbot and nginx

---
+++ certbot/scripts/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+trap exit INT TERM
+
+check_env() {
+  if [[ -z "${!1}" ]]; then
+    echo "$1 is not set"
+    exit 1
+  fi
+}
+
+check_env DOMAIN
+check_env EC2_CERTBOT_DIR
+check_env EC2_CERTBOT_BACKUP_DIR
+check_env EFS_BASE_CERTBOT_DIR
+
+certbot certonly \ 
+        --non-interactive \
+        --webroot \
+        --webroot-path /var/www/certbot/ \
+        --agree-tos \
+        --register-unsafely-without-email \
+        --domains "${DOMAIN}"
