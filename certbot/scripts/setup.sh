0a758e71335d3b2e7a4dcd2360a7ea9bd856a50e
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-08 11:43:21 +0700

    Test integration nginx and certbot

--- certbot/scripts/setup.sh
+++ certbot/scripts/setup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# set -e
+set -e
 
-# trap exit INT TERM
+trap exit INT TERM
 
-# check_env() {
-#   if [[ -z "${!1}" ]]; then
-#     echo "$1 is not set"
-#     exit 1
-#   fi
-# }
+check_env() {
+  if [[ -z "${!1}" ]]; then
+    echo "$1 is not set"
+    exit 1
+  fi
+}
 
-# check_env DOMAIN
-# check_env EC2_CERTBOT_DIR
-# check_env EC2_CERTBOT_BACKUP_DIR
-# check_env EFS_BASE_CERTBOT_DIR
+check_env DOMAIN
+check_env EC2_CERTBOT_DIR
+check_env EC2_CERTBOT_BACKUP_DIR
+check_env EFS_BASE_CERTBOT_DIR
 
-# certbot_dir="/etc/letsencrypt/keys"
-# dummy_dir="/etc/letsencrypt/live/$DOMAIN"
+certbot_dir="/etc/letsencrypt/keys"
+dummy_dir="/etc/letsencrypt/live/$DOMAIN"
 
-# if [ -d "$certbot_dir" ] && [ -n "$(ls -A "$certbot_dir")" ]; then
-#   echo "Deleting dummy certificates for $DOMAIN"
-#   rm -Rf "$dummy_dir"
-#   echo "Done. Creating real certificates"
-# fi
+if [ -d "$certbot_dir" ] && [ -n "$(ls -A "$certbot_dir")" ]; then
+  echo "Deleting dummy certificates for $DOMAIN"
+  rm -Rf "$dummy_dir"
+  echo "Done. Creating real certificates"
+fi
 
-sleep 10s
+sleep 5s
 
 certbot certonly --agree-tos --register-unsafely-without-email --domains "${DOMAIN}" --webroot --webroot-path /var/www/certbot
