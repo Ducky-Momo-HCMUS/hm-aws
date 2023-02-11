a814c8bf8ced150a92e157eab2387e0698067171
phineasla <63248715+phineasla@users.noreply.github.com>
2023-02-11 21:50:13 +0700

    Clean up

--- .platform/hooks/postdeploy/01_config_nginx.sh
+++ .platform/hooks/postdeploy/01_config_nginx.sh
@@ -4,7 +4,7 @@ load_nginx_config() {
     app_path="/var/app/current"
     config_path="$app_path/nginx/configs"
 
-    # Override alias
+    # Use "\" to override alias
     \cp --verbose -R $config_path/. "/etc/nginx/"
 }
 
